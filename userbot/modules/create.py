17c443519695b8a0b2c3a87d618f85041f2d17bf
Hüsü <73742151+husudu@users.noreply.github.com>
2026-04-21 16:08:06 +0400

    Refactor create.py for group and channel creation

--- userbot/modules/create.py
+++ userbot/modules/create.py
@@ -19,7 +19,7 @@ async def creategc(yarat):
             await yarat.client(functions.messages.DeleteChatUserRequest(chat_id=qrup_id, user_id="@BrendRobot"))
             invite = await yarat.client(functions.messages.ExportChatInviteRequest(peer=qrup_id))
             link = invite.link
-            await yarat.edit(f"[⚡ ʙʀᴇɴᴅ ᴜꜱᴇʀʙᴏᴛ](https://t.me/brenduserbot) vasitəsilə {ad} qrupu yaradıldı.\n\n🔘 Toxunaraq [{ad}]({link}) qrupuna qoşul.")
+            await yarat.edit(f"[⚡ ʙʀᴇɴᴅ ᴜꜱᴇʀʙᴏᴛ](https://t.me/brenduserbot) {ad} qrupunu yaratdı.\n\n🔘 [{ad}]({link}) qrupuna qoşul.")
         except Exception as e:
             await yarat.edit(f"❌ Xəta baş verdi: {e}")
     elif tip == "c":
@@ -27,10 +27,10 @@ async def creategc(yarat):
             link = await yarat.client(functions.channels.CreateChannelRequest(title=ad, about="⚡ Brend Userbot tərəfindən yaradıldı"))
             kanal_id = link.chats[0].id
             link = await yarat.client(functions.messages.ExportChatInviteRequest(peer=kanal_id))
-            await yarat.edit(f"[⚡ ʙʀᴇɴᴅ ᴜꜱᴇʀʙᴏᴛ](https://t.me/brenduserbot) vasitəsilə {ad} kanalı yaradıldı.\n\n🔘 Toxunaraq [{ad}]({link.link}) kanalına keçid et.")
+            await yarat.edit(f"[⚡ ʙʀᴇɴᴅ ᴜꜱᴇʀʙᴏᴛ](https://t.me/brenduserbot] {ad} kanalını yaratdı.\n\n🔘 [{ad}]({link.link}) kanalına keçid et.")
         except Exception as e:
-            await yarat.edit(f"❌ Xəta Baş verdi: {e}")
+            await yarat.edit(f"❌ Xəta baş verdi: {e}")
     else:
         await yarat.edit("Bu modulu işlətmə qaydasını bilmək üçün `.brend create` yazın")
 
-CmdHelp('create').add_command('create', '<g/c> <ad>', 'Cəmi bir əmrlə qrup və ya kanal yaradın qrup yaratmaq üçün .yarat q <ad> , kanal yaratmaq üçün .yarat k <ad> yazın.').add()
+CmdHelp('create').add_command('yarat', '<g/c> <ad>', 'Cəmi bir əmrlə qrup və ya kanal yaradın qrup yaratmaq üçün .yarat g <ad> , kanal yaratmaq üçün .yarat c <ad> yazın.').add()
